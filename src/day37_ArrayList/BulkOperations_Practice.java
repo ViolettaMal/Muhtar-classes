89da47e4e86c84607bdb54da6c1db67c3525f4e0
ViolettaMal <malakhovskaya@yahoo.com>
2020-08-02 11:12:32 -0500

    day37 class

--- src/day37_ArrayList/BulkOperations_Practice.java
+++ src/day37_ArrayList/BulkOperations_Practice.java
@@ -27,8 +27,20 @@ public class BulkOperations_Practice {
         Integer[] nums = {30, 25, 40, 15, 55, 65, 75, 85, 95, 100};
         numbers.addAll(Arrays.asList(nums));
         System.out.println(numbers);
+        System.out.println("====================");
 
+        ArrayList<String> group1 = new ArrayList<>();
+        //add all student names in your group
+        group1.addAll(Arrays.asList("Ahmet Asik", "Aigerim Kadyrbaeva", "Beyza Ozer", "Bledar Dakaj", "Edward Rojas",
+                "Gelila Woldie Woldie", "guljannat", "Kamol Nizomov", "mike", "Nurshat Naibi", "Olesea Codru",
+                "Omer Uslu","Ramazan Adam Sevindik", "Roman Boyko", "Said Ibrahimli", "Uladzislau Ihnatau", "Violetta Malakhovskaya", "Zulfiyya Moylamova" ));
 
+        // verify your mentor and one of your closest friend's name are contained in the list
+
+            boolean mentors = group1.containsAll(Arrays.asList("Bledar Dakaj", "Kamol Nizomov"));
+            boolean closeFriends = group1.containsAll(Arrays.asList("@Cristina Tiscenco", "@likavlasenko"));
+        System.out.println(mentors);
+        System.out.println(closeFriends);
 
 
 
