76d80a1bd256dbb6ecfb4b01b5a8a8a9d53cb101
ViolettaMal <malakhovskaya@yahoo.com>
2020-08-02 11:01:11 -0500

    day37 class

--- src/day37_ArrayList/BulkOperations_Practice.java
+++ src/day37_ArrayList/BulkOperations_Practice.java
@@ -1,7 +1,37 @@
 package day37_ArrayList;
 
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BulkOperations_Practice {
     public static void main(String[] args) {
+        ArrayList<String> students = new ArrayList<>();
+        students.add("Sayeem");
+        students.add("Waqar");
+        students.add("Beslan");
+        students.add("Dawud");
+        students.add("Ramazan");
+        students.add("Mehary");
+
+        //verify that the names Ulku, Busra are contained in students list
+
+        boolean r1 = students.containsAll(Arrays.asList("Ulku", "Busra", "Muhtar"));
+        System.out.println(r1);
+        System.out.println("===========================");
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        // add: 30, 25, 40, 15, 55, 65, 75, 85, 95, 100
+        Integer[] nums = {30, 25, 40, 15, 55, 65, 75, 85, 95, 100};
+        numbers.addAll(Arrays.asList(nums));
+        System.out.println(numbers);
+
+
+
+
+
 
     }
 }
